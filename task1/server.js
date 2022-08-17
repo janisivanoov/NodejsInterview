70ad95356924b9818165e7b73a5712ea4aaede23
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-08-17 13:18:12 +0300

    Update server.js
    Co-authored-by: Ivan Kosiakov <ivan0kosyakov@gmail.com>

--- task1/server.js
+++ task1/server.js
@@ -5,6 +5,7 @@ const requestHandler = (request, response) => {
 console.log(request.url);
 response.end('Hello Node.js Server!');
 };
+
 const server = http.createServer(requestHandler);
 server.listen(port, (err) => {
 if (err) { return console.log('something bad happened', err); }
